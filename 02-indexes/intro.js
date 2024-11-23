c2c1a919d172ff038269a3a4c53b51edc59eae20
Hadar4476 <hadar4476@gmail.com>
2024-11-23 13:16:08 +0200

    changes

--- 02-indexes/intro.js
+++ 02-indexes/intro.js
@@ -51,7 +51,7 @@
 
 // WHAT IS THE DIFFERENCE BETWEEN COMPOUND AND PARTIAL INDEX?
 // it is in the size of the PARTIAL INDEX which stores only specific fields with an option of filtering - better for performance.
-// this is also depends on the scenario for the data we want to get.
+// this is also depends on the scenario for the data you want to get.
 
 // UNIQUE PARTIAL INDEX
 // by running "db.someCollection.createIndex({email: 1}, {unique: true})" a unique index for the email field is created.
@@ -61,7 +61,7 @@
 // for this a PARTIAL INDEX can be handy by running "db.someCollection.createIndex({}, { unique: true, partialFilterExpression: { email: {$exists} } })"
 // this will create an index only if the "email" field is not null.
 
-// TIME-TO-LIVE INDEX(TTL) - DATA CLEANUP
+// TIME-TO-LIVE(TTL) INDEX - DATA CLEANUP
 // this index can work only on a single field, which have to be a DATE type like createdAt or updatedAt and can't work with COMPOUND INDEX.
 // what it does is deleting a document after a certain time and it works by running "db.someCollection.createIndex({ createdAt: 1 }, { expireAfterSeconds: 10 })"
 // this will all documents after 10 seconds.
@@ -74,3 +74,47 @@
 // there is a way to optimize the queries to retrieve only the desired field's value by running
 // "db.someCollection.explain("executionStats").find({ "SOME_FIELD": "SOME_VALUE" }, { _id: 0, "SOME_FIELD": 1})" this will prevent the whole document examation
 // and will return only the field which is indexed.
+
+// MULTI KEY INDEX
+// an index that hold all the values of a field of type array.
+// by running "db.someCollection.explain("executionStats").find({"SOME_ARRAY_FIELD": "SOME_VALUE"})" there is a "isMultiKey: true" field.
+// each value in the array is stored separately in the index list.
+// if array contains only objects it is best to use a key of that object as an index because the index list will be large and cost more
+// performance than it already is cost.
+// you can't create a compound index of multiple arrays like this: db.someColllection.createIndex({"SOME_ARRAY": 1, "SOME_OTHER_ARRAY": 1})
+
+// TEXT KEY INDEX - INTRO
+// this type of index is good when you want to get data depending on a string which is a part of multiple strings like: description or large title.
+// typically you will use regex with "db.collectionName.find({ "SOME_FIELD": { $regex: /<SOME_VALUE>/, $options: 'i' } })"
+// you create a text index by running "db.someCollection.createIndex({"SOME_FIELD": "text"})" using "text" instead of 1 here is important.
+// you find a result by running: "db.someCollection.find({ $text: { $search: "SOME_VALUE" }})", casing is not important as it stores the values in lowercase.
+// when finding, you don't use the field name because text indexes are expensive for performance.
+// if the "SOME_VALUE" contains more than 2 words, the index list will return results which contains each word separatley.
+// if you want to get a whole string instead of treating each word separatley you can run
+// "db.someCollection.find({ $text: { $search: "\"SOME_VALUE_WITH_MULTIPLE_WORDS"\" } })"
+
+// TEXT KEY INDEX - SORTING
+// lets say you get 2 results for using "db.someCollection.find({ $text: { $search: "SOME_VALUE SOME_OTHER_VALUE" } })" but the first result contain only "SOME_VALUE"
+// and the second result contains both "SOME_VALUE" and "SOME_OTHER_VALUE", the second result should be prioritize, MongoDB gives a score to each result which is based on
+// how much does that string match.
+// you can use it for sorting by running
+// "db.someCollection.find({ $text: { $search: "SOME_VALUE SOME_OTHER_VALUE" }}, { score: { $meta: "textScore" }}).sort({ score: { $meta: "textScore" }})"
+
+// TEXT KEY INDEX - COMBINATION
+// by running "db.someCollection.createIndex({ "SOME_FIELD": "text", "SOME_OTHER_FIELD": "text" })" you can create a text index for multiple fields.
+
+// TEXT KEY INDEX - LANGUAGE
+// when using the text index, MongoDB won't search for stop words like: "is", "a", "the" etc.
+// this changes from language to language, so you can define the language for the text index by running
+// "db.someCollection.createIndex({"SOME_FIELD": "text"}, {default_language: "english"})", there is a limit to the supported languages.
+// English is the default and Hebrew is not supported.
+
+// TEXT KEY INDEX - WEIGHT
+// weight is a way to prioritize a field which is indexed with another another field.
+// by running "db.someCollection.createIndex({"SOME_FIELD": "text", "SOME_OTHER_FIELD": "text"}, { weights: { "SOME_FIELD": 1, "SOME_OTHER_FIELD": 10 }})"
+// this will make sure that that "SOME_OTHER_FIELD" is prioritize when finding.
+// it also work with the scoring of MongoDB, see TEXT KEY INDEX - SORTING for more details.
+
+// BUILDING INDEX IN PRODUCTION
+// building indexes might take a while, for that you can run "db.someCollection.createIndex({ "SOME_FIELD": 1}, { background: true})"
+// this will make sure that the process of inserting or reading won't depend on the creation of an index if the creation will take a while.
